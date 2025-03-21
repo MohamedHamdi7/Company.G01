9d7ec53827016a99d12e5d872743cdfe5110f5ce
ahmed <ahmed@DESKTOP-LSUT3NK>
2025-03-22 01:51:15 +0200

    Refactoring

--- Company.G01.BLL/Repositories/EmployeeRepository.cs
+++ Company.G01.BLL/Repositories/EmployeeRepository.cs
@@ -9,47 +9,47 @@ using Company.G01.DAL.Models;
 
 namespace Company.G01.BLL.Repositories
 {
-    public class EmployeeRepository : IEmployeeRepository
+    public class EmployeeRepository :GenericRepository<Employee>, IEmployeeRepository
     {
-        private readonly CompanyDbContext context;
-        public EmployeeRepository(CompanyDbContext _context)
+        //private readonly CompanyDbContext context;
+        public EmployeeRepository(CompanyDbContext _context):base(_context)
         {
-            context = _context;
+            //context = _context;
         }
 
-        IEnumerable<Employee> IEmployeeRepository.GetAll()
-        {
-            return context.Employees.ToList();
-        }
+        //IEnumerable<Employee> IEmployeeRepository.GetAll()
+        //{
+        //    return context.Employees.ToList();
+        //}
 
-        Employee IEmployeeRepository.Get(int id)
-        {
-            return context.Employees.Find(id);
-        }
+        //Employee IEmployeeRepository.Get(int id)
+        //{
+        //    return context.Employees.Find(id);
+        //}
 
-        int IEmployeeRepository.Add(Employee model)
-        {
-            context.Employees.Add(model);
-            return context.SaveChanges();
-        }
+        //int IEmployeeRepository.Add(Employee model)
+        //{
+        //    context.Employees.Add(model);
+        //    return context.SaveChanges();
+        //}
 
-        int IEmployeeRepository.Update(Employee model)
-        {
-            context.Employees.Update(model);
-            return context.SaveChanges();
-        }
+        //int IEmployeeRepository.Update(Employee model)
+        //{
+        //    context.Employees.Update(model);
+        //    return context.SaveChanges();
+        //}
 
 
-        int IEmployeeRepository.Delete(Employee model)
-        {
-            context.Employees.Remove(model);
-            return context.SaveChanges();
-        }
+        //int IEmployeeRepository.Delete(Employee model)
+        //{
+        //    context.Employees.Remove(model);
+        //    return context.SaveChanges();
+        //}
 
-       
 
-        
 
-        
+
+
+
     }
 }
---
+++ Company.G01.BLL/Repositories/GenericRepository.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Company.G01.BLL.Interfaces;
+using Company.G01.DAL.Data.Contexts;
+using Company.G01.DAL.Models;
+
+namespace Company.G01.BLL.Repositories
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : BaseEntity
+    {
+        private readonly CompanyDbContext context;
+        public GenericRepository(CompanyDbContext _context)
+        {
+            context= _context;
+
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+           return context.Set<T>().ToList();
+        }
+
+        public T? Get(int id)
+        {
+            return context.Set<T>().Find(id);
+        }
+
+        public int Add(T model)
+        {
+            context.Set<T>().Add(model);
+            return context.SaveChanges();
+        }
+        public int Update(T model)
+        {
+            context.Set<T>().Update(model);
+            return context.SaveChanges();
+        }
+        public int Delete(T model)
+        {
+            context.Set<T>().Remove(model);
+            return context.SaveChanges();
+        }
+
+        
+
+       
+
+        
+    }
+}
